f25a6f8bd3190f76273fb41892643e3801e9cf32
Mike <michael.obsessvr.com>
2020-02-11 12:56:06 -0500

    Implemented About component and prop details

---
+++ src/About.svelte
@@ -0,0 +1,13 @@
+<script>
+  // this is a prop that the component will be expecting - using the export keyword
+  export let about;
+</script>
+
+<style>
+  h2 {
+    color: blue;
+    font-size: 14px;
+  }
+</style>
+
+<h2>The about component: {about}</h2>
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+  import About from "./About.svelte";
+  export let name;
+
+  let line1 = "This about prop is being rendered in the About component";
+  let line2 =
+    "Another About component with a differnet prop state. Should use export to expect a prop within the component";
+  let line3 =
+    "One interesting thing about Svelte is that the stylings within the component is contained - meaning the styling outside of the encapuslated component will not leak into it's children components";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <About about={line1} />
+  <About about={line2} />
+  <About about={line3} />
+</main>
